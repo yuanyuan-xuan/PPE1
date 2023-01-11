a736ea9b48b6f50f32b8ed581fcab559c22a5258
yuanyuan-xuan <yuanyuan.xuan@sorbonne-nouvelle.fr>
2023-01-11 01:21:31 +0100

    commit les exercices_Itrameur

---
+++ exercices_Itrameur/base_iTrameur.sh
@@ -0,0 +1,24 @@
+<lang="fr">
+<page="fr-1">
+<text>Ici, le contenu du fichier fr-1.</text>
+</page> §
+<page="fr-2">
+<text>Ici, le contenu du fichier fr-2.</text>
+</page> §
+</lang>
+<lang="en">
+<page="en-1">
+<text>Ici, le contenu du fichier en-1.</text>
+</page> §
+<page="en-2">
+<text>Ici, le contenu du fichier en-2.</text>
+</page> §
+</lang>
+<lang="ch">
+<page="ch-1">
+<text>Ici, le contenu du fichier ch-1.</text>
+</page> §
+<page="ch-2">
+<text>Ici, le contenu du fichier ch-2.</text>
+</page> §
+</lang>
